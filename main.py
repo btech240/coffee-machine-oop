fa82cff88853b9698e40ab5abb7d42edd57b2946
btech240 <brenttlong@gmail.com>
2024-01-05 10:00:32 -0500

    Fixed item not found bug

--- main.py
+++ main.py
@@ -18,6 +18,10 @@ while not is_off:
     elif user_input == "report":
         coffee_machine.report()
         money_machine.report()
+    elif menu.find_drink(user_input) == None:
+        # Without this logic, the code would crash if you requested an item that didn't exist
+        # it would continue to look for ingredients for a None type object
+        pass
     else:
         drink = menu.find_drink(user_input)
         if coffee_machine.is_resource_sufficient(drink) and money_machine.make_payment(drink.cost):
